6e708c084c66fe2c74b7b052f7d5c6b0a83df83c
kimgihye <kimgihye55@gmail.com>
2025-03-13 11:22:51 +0900

    study_05 todo-list 할일목록 추가

---
+++ study_05_event/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import ExArr from "./components/ExArr.vue";
+import Event from "./components/Event.vue";
+import { ref } from "vue";
+const showEx = ref(false);
+</script>
+
+<template>
+  <div>
+    <ExArr />
+    <button @click="showEx = !showEx">
+      {{ showEx ? "닫기" : "ExArr 보기" }}
+    </button>
+    <Event  v-if="showEx"/>
+   
+  </div>
+</template>
+
+<style scoped></style>
---
+++ study_05_event/src/components/ExArr.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref } from "vue";
+import TodoList from "./TodoList.vue";
+// TodoList 화면 보이게
+const showTodoList = ref(false);
+</script>
+<template>
+  <div class="wrap">
+    <div class="container">
+      <h1>TodoList Ex</h1>
+      <button @click="showTodoList = !showTodoList">
+        {{ showTodoList ? "닫기" : "TodoList 보기" }}
+      </button>
+      <TodoList v-if="showTodoList" />
+    </div>
+    <hr />
+    <div class="container">
+      <h1>Array 공부</h1>
+    </div>
+  </div>
+</template>
+<style scoped>
+.wrap{
+  background-color: antiquewhite;
+  display: flex;
+  justify-content: center;
+}
+.container{
+  padding: 20px;
+  flex: 1;
+  text-align: center;
+}
+</style>
